0bd87585e0cf670c4f1c6806dcad2b49190c8884
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-02-27 22:56:11 +0900

    ルーティングテーブル作成

--- public/index.php
+++ public/index.php
@@ -15,4 +15,14 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+// echo get_class($router);
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+    
+// Display the routing table
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
