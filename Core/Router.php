473d867b927351fea34aaa1817ae17bd9ed2694f
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-02-28 00:04:15 +0900

    基本のルートマッチ

--- Core/Router.php
+++ Core/Router.php
@@ -36,4 +36,32 @@ class Router
     {
         return $this->routes;
     }
+
+    /**
+     * Match the route to the routes in the routing table, setting the $paeams property if a route is found.
+     * 
+     * @param string $url The route URL
+     * 
+     * @return boolean true if a match found, false otherwise
+     */
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get the currently matched parameters
+     * 
+     * @return array
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -21,8 +21,18 @@ $router = new Router();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
-    
-// Display the routing table
-echo '<pre>';
-var_dump($router->getRoutes());
-echo '</pre>';
+
+// // Display the routing table
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// echo '</pre>';
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo 'No route found for URL ' . $url;
+}
