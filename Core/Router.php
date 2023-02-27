34832ad98d55e43ce817a4bf5757f8977afa55e6
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-02-28 00:38:07 +0900

    preg_matchを用いたルートマッチ

--- Core/Router.php
+++ Core/Router.php
@@ -46,12 +46,24 @@ class Router
      */
     public function match($url)
     {
-        foreach ($this->routes as $route => $params) {
-            if ($url == $route) {
-                $this->params = $params;
-                return true;
+        // foreach ($this->routes as $route => $params) {
+        //     if ($url == $route) {
+        //         $this->params = $params;
+        //         return true;
+        //     }
+        // }
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?<action>[a-z-]+)$/";
+        if (preg_match($reg_exp, $url, $matches)) {
+            $params = [];
+            foreach ($matches as $key => $match) {
+                if (is_string($key)) {
+                    $params[$key] = $match;
+                }
             }
+            $this->params = $params;
+            return true;
         }
+
         return false;
     }
 
